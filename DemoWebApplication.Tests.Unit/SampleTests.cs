2c0ba5554cc045d8ae6af1035b0731c7d75f7e00
Marc Müller <mmueller@4tecture.ch>
2015-11-17 21:11:34 +0100

    Initial commit

---
+++ DemoWebApplication.Tests.Unit/SampleTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoWebApplication.Tests.Unit
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void SampleUnitTest1()
+        {
+        }
+
+        [TestMethod]
+        public void SampleUnitTest2()
+        {
+        }
+
+        [TestMethod]
+        public void SampleUnitTest3()
+        {
+        }
+
+        [TestMethod]
+        public void SampleUnitTest4()
+        {
+        }
+
+        [TestMethod]
+        public void SampleUnitTest5()
+        {
+        }
+    }
+}
